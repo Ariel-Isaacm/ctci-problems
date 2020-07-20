eb3b9604af589bb8ea6e15a1da41d2e14481317b
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-07-20 16:31:02 -0700

    Meaning less commit cause it's my birthday 🎉

--- main/java/LinkedLists/LinkedLists.java
+++ main/java/LinkedLists/LinkedLists.java
@@ -180,7 +180,7 @@ public class LinkedLists {
 
     //2.7 Intersection: Given two (singly) linked lists, determine if the two lists intersect. Return the intersecting node.
     // Note that the intersection is defined based on reference, not value.
-    // That is, if the kth node of the first linked list is the exact same node (by reference) as the jth node of the second
+    // That is, if the kth node of the first linked list is the exact same node(by reference) as the jth node of the second
     //linked list, then they are intersecting.
     public ListNode intersection(ListNode node1, ListNode node2) {
         HashSet<ListNode> seen = new HashSet<ListNode>();
